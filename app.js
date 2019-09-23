0fccd84ca3253128714d299086d158343035b6d6
Alexander Rendon Cadavid <backend2@eyepaycard.co>
2019-09-23 11:39:04 -0500

    cancel app.listen

--- app.js
+++ app.js
@@ -144,6 +144,6 @@ const commands = [
   "capacitor"
 ];
 
-app.listen(process.env.PORT || 5000, () =>
-  console.log(`"Telegram bot is listening on port ${process.env.PORT}!`)
-);
+// app.listen(process.env.PORT || 5000, () =>
+//   console.log(`"Telegram bot is listening on port ${process.env.PORT}!`)
+// );
