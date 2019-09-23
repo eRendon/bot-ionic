da3631812c177c1ebaeff352d50d1a56046e716d
Alexander Rendon Cadavid <backend2@eyepaycard.co>
2019-09-23 18:14:20 -0500

    Update app.js

--- app.js
+++ app.js
@@ -96,11 +96,11 @@ bot.onText(/\/*/, async msg => {
 //   // await bot.sendMessage(msg.chat.id, `Bienvenido ${msg.chat.username}. Por favor, escribe al privado "comandos" y te contaré qué puedes aprender de mi`);
 // });
 //
-// bot.onText(/\/*comandos/, msg => {
-//   commands.forEach(async command => {
-//     await bot.sendMessage(msg.chat.id, command);
-//   });
-// });
+bot.onText(/\/*comandos/, msg => {
+  commands.forEach(async command => {
+    await bot.sendMessage(msg.chat.id, command);
+  });
+});
 
 bot.onText(/\/echo (.+)/, async (msg, match) => {
   console.log(msg);
