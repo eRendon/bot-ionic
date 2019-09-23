baf8cae7d8ec1d2eac22ee10b375ae2bbe23a192
Alexander Rendon Cadavid <backend2@eyepaycard.co>
2019-09-23 15:21:27 -0500

    add tutorial capacitor

--- controllers/tutorials/index.js
+++ controllers/tutorials/index.js
@@ -7,7 +7,8 @@ module.exports = msg => {
     }
 };
 const tutorialsCapacitor = [
-    "Tutorial de ionic 4 + capacitor  https://www.youtube.com/watch?v=MTuttuME414"
+    "Tutorial de ionic 4 + capacitor  https://www.youtube.com/watch?v=MTuttuME414",
+    'MeetUp capacitor whit ionic developers team https://www.youtube.com/watch?v=tDW2C6rcH6M'
 ];
 
 
